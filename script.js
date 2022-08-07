72c9007a19e1524102cd1494445d3be6ca1d9346
kaykyjustiniano <kaikiatg@gmail.com>
2022-08-07 13:45:29 -0300

    Nova_atualizacao

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-document.getElementsByClassName("butao").style.color = "blue";
+var select = document.querySelector('select');
+var aparecerTela = document.querySelector('p');
+
+select.addEventListener('change', Horario);
+
+function Horario() {
+    var pegou = select.value;
+
+    if (pegou === 'Manhã'){
+        aparecerTela.textContent = 'deu certo aqui ooo'
+    }
+    else if (pegou === 'Tarde'){
+        aparecerTela.textContent = 'deu certo aqui';
+    }
+    else if (pegou === 'Noite'){
+        aparecerTela.textContent = 'deu certo';
+    }
+    else if (pegou === 'Madrugada'){
+        aparecerTela.textContent = 'deu';
+    }
+   
+}
+
